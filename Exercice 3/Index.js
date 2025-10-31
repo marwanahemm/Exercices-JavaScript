6de4aa644be5ae37e03095bd878442cbcccf031c
marwanahemm <marwan.ahemmane@gmail.com>
2025-10-31 14:27:58 +0100

    Add let number and console.log

---
+++ Exercice 3/Index.js
@@ -0,0 +1,7 @@
+let nombre1 = prompt("Entrez le premier nombre :");
+
+let nombre2 = prompt("Entrez le deuxième nombre :");
+
+let somme = Number(nombre1) + Number(nombre2);
+
+console.log("La somme est : " + somme);
