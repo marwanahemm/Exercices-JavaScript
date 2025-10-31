c0e5da785e10a7d727c94fbfe8da1852d48ff911
marwanahemm <marwan.ahemmane@gmail.com>
2025-10-31 15:48:41 +0100

    Add let number & console.log

---
+++ Exercice 5/Index.js
@@ -0,0 +1,7 @@
+let number = prompt("Enter a number:");
+number = Number(number);
+if (number % 2 === 0) {
+  console.log("The number " + number + " is even.");
+} else {
+  console.log("The number " + number + " is odd.");
+}
